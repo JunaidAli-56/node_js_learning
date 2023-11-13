5c76728bf917387c04771c14c30ca9c6e4b34443
Junaid <junaidhayat33303@gmail.com>
2023-11-13 22:25:37 +0500

    another commit

--- fsAsync/index.js
+++ fsAsync/index.js
@@ -7,3 +7,7 @@ fs.writeFile('read.txt', 'Hello jarviz this is the async read.txt....', (err) =>
 fs.appendFile('read.txt', '  Hello Append text', (err) => {
     console.log("append task complete")
 })
+
+fs.readFile('read.txt', 'UTF-8', (err, data) => {
+    console.log(data)
+})
