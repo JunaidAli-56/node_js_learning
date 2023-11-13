0f318147f2917d29f68984f44da0d1668f827178
Junaid <junaidhayat33303@gmail.com>
2023-11-13 21:08:22 +0500

    fs commits

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("helo WOrld")
+const fs = require("fs");
+
+// Create a new file
+fs.writeFileSync('read.txt', 'hello Jarviz, can you hear me.');
+fs.appendFileSync('read.txt', ' Alright lets do some work togther. Are you ready?');
