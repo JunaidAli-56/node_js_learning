da64f4aaf7a25fb745a58957023414ddaa617948
Junaid <junaidhayat33303@gmail.com>
2023-11-13 22:03:14 +0500

    another commit

--- fs_core/index.js
+++ fs_core/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+// *  make directory mkdir and for remove rmdir method
 // Create a new file
 fs.writeFileSync('read.txt', 'hello Jarviz, can you hear me.');
 fs.appendFileSync('read.txt', ' Alright lets do some work togther. Are you ready?');
