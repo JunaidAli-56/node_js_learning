96f14848b380b0ab59010030ba4625c485c8df50
Junaid <junaidhayat33303@gmail.com>
2023-11-13 21:15:23 +0500

    read commit

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const fs = require("fs");
 // Create a new file
 fs.writeFileSync('read.txt', 'hello Jarviz, can you hear me.');
 fs.appendFileSync('read.txt', ' Alright lets do some work togther. Are you ready?');
+
+// read data 
+// remember the buff concept
+const buff_data = fs.readFileSync('read.txt');
+original_data = buff_data.toString();
+console.log(original_data);
