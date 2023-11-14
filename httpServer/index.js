5ff6f955b5305498f7b301a66cc30cb2295835fc
Junaid <junaidhayat33303@gmail.com>
2023-11-14 23:55:04 +0500

    another commit

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const { type } = require('os');
 const port = 8000;
 const server = http.createServer((req, res) => {
-    res.end('Hello Jarviz from another world');
+    if (req.url == '/') {
+        res.end("Hello Jarviz: Home Page")
+    } else if (req.url == '/about') {
+        res.end("about page")
+    } else if (req.url == '/contact') {
+        res.end("contact page")
+    } else {
+        res.writeHead(404, { "Content-type": "text/html" })
+        res.end("<h1>404 Nothing here</h1>")
+    }
 })
 
 server.listen(port, "127.0.0.1", () => {
