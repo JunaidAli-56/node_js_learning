298186d4852ab4f6244592931ba68918e8a098ef
Junaid <junaidhayat33303@gmail.com>
2023-11-14 22:31:09 +0500

    another commit

---
+++ npmModule/index.js
@@ -0,0 +1,8 @@
+// const chalk = require("chalk")
+// // 
+// const name = chalk.blue('Juniad')
+// console.log(name)
+// console.log('Hello world!');
+
+import chalk from ("chalk")
+console.log(chalk.blue('Juniad'))
