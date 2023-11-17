08f106700394cd10d4871a41306f44fd4ab0d3b8
Junaid <junaidhayat33303@gmail.com>
2023-11-17 23:34:45 +0500

    another commit

--- express_jarviz/nav.js
+++ express_jarviz/nav.js
@@ -3,10 +3,26 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-    res.send('welcome to the Home Page')
+    res.write('<h1>welcome to the Home Page</h1>')
+    res.write('<h2>welcome to the Heading 2 Page</h2>')
+    res.send();
 })
 app.get('/about', (req, res) => {
     res.status(200).send('welcome to the ABout Page')
+})
+
+// if you use json( ) instead of send() it will convert non-object into object...
+app.get('/temp', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name: 'jarviz'
+        },
+        {
+            id: 2,
+            name: 'firday'
+        },
+    ])
 })
 
 app.listen(port, () => {
